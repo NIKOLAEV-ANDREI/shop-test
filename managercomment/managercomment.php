d417a289b4c59e481ccd3840c1f74fe9407582a8
ItsNotUber <114165477+NIKOLAEV-ANDREI@users.noreply.github.com>
2026-08-19 13:41:49 +0300

    Добавлена установка таблицы комментариев менеджеров

--- managercomment/managercomment.php
+++ managercomment/managercomment.php
@@ -26,13 +26,62 @@ class ManagerComment extends Module
         $this->description = $this->l('A skeleton module for manager comments on orders.');
     }
 
+    //МЕТОД СОЗДАНИЯ ТАБЛИЦЫ
+    private function installDatabase()
+    {
+        $sql = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'manager_comment` (
+            `id_manager_comment` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+            `id_order` INT(10) UNSIGNED NOT NULL,
+            `id_employee` INT(10) UNSIGNED NOT NULL,
+            `employee_name` VARCHAR(255) NOT NULL,
+            `comment` VARCHAR(500) NOT NULL,
+            `date_add` DATETIME NOT NULL,
+            `date_upd` DATETIME NOT NULL,
+            PRIMARY KEY (`id_manager_comment`),
+            KEY `idx_order_date` (`id_order`, `date_add`),
+            KEY `idx_date_add` (`date_add`),
+            KEY `idx_employee` (`id_employee`)
+        ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;';
+
+        return Db::getInstance()->execute($sql);
+    }
+
+    //МЕТОД УДАЛЕНИЯ ТАБЛИЦЫ
+    private function uninstallDatabase()
+    {
+        $sql = 'DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'manager_comment`';
+
+        return Db::getInstance()->execute($sql); //получение подключения PrestaShop к базе + возвращение результата выполнения
+    }
+
     public function install()
     {
-        return parent::install();
+        if (!parent::install()) {
+            return false;
+        }
+
+        if (!$this->installDatabase()) {
+            parent::uninstall();
+
+            return false;
+        }
+
+        if (!$this->registerHook('displayAdminOrder')) {
+            $this->uninstallDatabase();
+            parent::uninstall();
+
+            return false;
+        }
+
+        return true;
     }
 
     public function uninstall()
     {
+        if (!$this->uninstallDatabase()) {
+            return false;
+        }
+
         return parent::uninstall();
     }
 }
